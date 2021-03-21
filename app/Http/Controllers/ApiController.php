dc73c7b686c778166815166be356871bd602adc8
Temerlan <190103239@stu.sdu.edu.kz>
2021-03-21 22:51:45 +0600

    apiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Client;
+
 class ApiController extends Controller
 {
-    //
+    /*
+    This function return that all client rows
+    */
+    public function index(Request $request){
+        $clients = Client::all();
+
+        return response($clients, 200);
+    }
 }
